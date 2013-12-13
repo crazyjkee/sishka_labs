69d9b35e6654d6ac12a8d08b98bc1886e8fe0168
artem <crazyjkee@gmail.com>
2013-12-13 17:27:31 +0400

    lab_1

---
+++ lab_1/src/lab_1.c
@@ -0,0 +1,136 @@
+/*
+ ============================================================================
+ Name        : lab_1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void n1(){
+	printf("     \n\n\n\n                     **********************************\n ");
+	printf("                    *Базовые типы данных и ввод-вывод*\n");
+	printf("                     *   Группа 2311б:Name lastname   *\n");
+	printf("                     **********************************\n\n");
+}
+
+void n2(){
+	int i=0;
+	printf(" xxxxxxxxxxxxxxxxxxxx\n");
+	for(i=0;i<7;i++)
+	printf(" x                  x\n");
+	printf(" xxxxxxxxxxxxxxxxxxxx\n\n");
+	printf(" xxxxxxxxxxxxxxxxxxxx\n");
+	for(i=0;i<12;i++)
+	printf(" x                  x\n");
+	printf(" xxxxxxxxxxxxxxxxxxxx\n\n");
+	printf(" x\n");
+	printf(" x  x\n");
+	printf(" x    x\n");
+	printf(" x      x\n");
+	printf(" x        x\n");
+	printf(" x          x\n");
+	printf(" xxxxxxxxxxxxxx\n\n");
+	printf("        x\n");
+	printf("       x x\n");
+	printf("      x   x\n");
+	printf("     x     x\n");
+	printf("    x       x\n");
+	printf("   x         x\n");
+	printf("  xxxxxxxxxxxxx\n\n");
+	printf("           x\n");
+	printf("         x   x\n");
+	printf("       x       x\n");
+	printf("     x           x\n");
+	printf("       x       x\n");
+	printf("         x   x\n");
+	printf("           x\n\n");
+	printf("        x\n");
+	printf("        x x\n");
+	printf("        x  x\n");
+	printf("        x   x\n");
+	printf("        x    x\n");
+	printf("        x     x\n");
+	printf("        x      x\n");
+	printf("        x       x\n");
+	printf("        x     x\n");
+	printf("        x   x\n");
+	printf("        x x\n");
+	printf("        x\n\n");
+
+	printf("        x\n");
+	printf("      x   x\n");
+	printf("    x       x\n");
+	printf("  x           x\n");
+	printf("xxxxxxxxxxxxxxxx\n\n");
+
+	printf("           x\n");
+	printf("          x   x\n");
+	printf("         x       x\n");
+	printf("        x           x\n");
+	printf("       x            x\n");
+	printf("      x            x \n");
+	printf("     x          x  \n");
+	printf("    x        x      \n");
+	printf("   x      x\n");
+	printf("  x    x\n");
+	printf(" x  x\n");
+	printf("x\n\n");
+
+	printf("        x                      x\n");
+	printf("       x x                  x x \n");
+	printf("      x   x             x    x\n");
+	printf("     x     x        x       x\n");
+	printf("    x       x   x          x\n");
+	printf("   x        x x           x\n");
+	printf("  x     x        x       x\n");
+	printf(" x  x               x   x\n");
+	printf("x                      x\n\n");
+
+	printf("        xxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+	printf("       x                          x\n");
+	printf("      x                          x\n");
+	printf("     x                          x\n");
+	printf("    x                          x\n");
+	printf("   x                          x\n");
+	printf("  x                          x\n");
+	printf(" xxxxxxxxxxxxxxxxxxxxxxxxxxxx\n\n");
+
+	printf("                   x\n");
+	printf("                x    x\n");
+	printf("             x         x\n");
+	printf("          x              x\n");
+	printf("       x  x  x  x  x  x  x x\n");
+	printf("          x              x\n");
+	printf("             x         x\n");
+	printf("                x    x\n");
+	printf("                   x\n\n");
+
+	printf("                __        __     __ \n");
+	printf("      /|       |__|      |__|   |__\n ");
+	printf("      |        __|       __|    __|\n\n");
+
+	printf("          _____________________\n");
+	printf("         /    _____      _____   \ \n");
+	printf("        /    |     |    |     |    \ \n");
+	printf(" ______/     |_____|    |_____|      \______________ \n");
+	printf("|                                                   | \n");
+	printf("|___________________________________________________| \n");
+	printf("          /    \                        /    \ \n");
+	printf("          \____/                        \____/ \n\n");
+
+
+
+
+
+}
+
+int main(void) {
+	n1();
+	n2();
+	return EXIT_SUCCESS;
+}
